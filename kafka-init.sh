a00534fc58675a93942d75e62d4e39aa5d0db7c6
alessioRAG <101555375+alessioRAG@users.noreply.github.com>
2025-02-19 20:43:20 +0100

    Delete kafka-init.sh

--- kafka-init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sleep 10
-
-kafka-topics --create --bootstrap-server kafka:9092 --replication-factor 1 --partitions 1 --topic datiRiot || echo "Topic già creato o errore"
-
-exec "$@"
